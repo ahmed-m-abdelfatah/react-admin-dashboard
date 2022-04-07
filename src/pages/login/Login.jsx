da74456b0d92342fa440393155340f16b2587938
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2022-04-07 21:33:24 +0200

    https://youtu.be/D9W7AFeJ3kk?t=698

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,65 @@
 import './login.scss';
 
+import { useState } from 'react';
+import Joi from 'joi';
+
 const Login = () => {
   console.log('~ Login');
 
+  const [error, setError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  function handelChange(e) {
+    setError(false);
+    setErrorMsg('');
+    return setUser({ ...user, [e.target.name]: e.target.value });
+  }
+
+  function validateForm(user) {
+    let schema = Joi.object({
+      email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+        .required(),
+
+      password: Joi.string().required(),
+    });
+
+    return schema.validate(user, { abortEarly: true });
+  }
+
+  function handelSubmit(e) {
+    e.preventDefault();
+    let validationResult = validateForm(user);
+    if (validationResult.error) {
+      setError(true);
+      setErrorMsg(validationResult.error.message);
+    } else {
+      // TODO firebase
+    }
+    console.log('~ TODO firebase');
+  }
+
   return (
     <div className='login'>
-      <form>
-        <input type='email' placeholder='Email' />
-        <input type='password' placeholder='Password' />
+      <form onSubmit={handelSubmit}>
+        <input
+          onChange={handelChange}
+          type='email'
+          placeholder='Email'
+          name='email'
+        />
+        <input
+          onChange={handelChange}
+          type='password'
+          placeholder='Password'
+          name='password'
+        />
         <button type='submit'>Login</button>
+        {error && <span>{errorMsg.replace(/"/g, '')}</span>}
       </form>
     </div>
   );
