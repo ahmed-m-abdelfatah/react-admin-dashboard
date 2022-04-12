407633bebea8dab8bf2801b4ec8b8796cb71fca8
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2022-04-12 22:32:43 +0200

    solve login error

--- src/firebase.js
+++ src/firebase.js
@@ -9,6 +9,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+// console.log('~ firebaseConfig', firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
